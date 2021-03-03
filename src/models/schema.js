5fbf931565bbc5f25f3ed0a98aa747d192a69861
lennos36 <shane.lennon36@mail.dcu.ie>
2021-03-03 22:20:34 +0000

    first commit

---
+++ src/models/schema.js
@@ -0,0 +1,48 @@
+export const schema = {
+    "models": {
+        "Chatty": {
+            "name": "Chatty",
+            "fields": {
+                "id": {
+                    "name": "id",
+                    "isArray": false,
+                    "type": "ID",
+                    "isRequired": true,
+                    "attributes": []
+                },
+                "user": {
+                    "name": "user",
+                    "isArray": false,
+                    "type": "String",
+                    "isRequired": true,
+                    "attributes": []
+                },
+                "message": {
+                    "name": "message",
+                    "isArray": false,
+                    "type": "String",
+                    "isRequired": true,
+                    "attributes": []
+                },
+                "createdAt": {
+                    "name": "createdAt",
+                    "isArray": false,
+                    "type": "AWSDateTime",
+                    "isRequired": false,
+                    "attributes": []
+                }
+            },
+            "syncable": true,
+            "pluralName": "Chatties",
+            "attributes": [
+                {
+                    "type": "model",
+                    "properties": {}
+                }
+            ]
+        }
+    },
+    "enums": {},
+    "nonModels": {},
+    "version": "9bce235ea8e39e7c599bae8f040f9fe5"
+};
